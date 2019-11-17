87b4998d265ab5181c1e63c42dd28ccbd846b09c
Foxomat <Fox.ket@freenet.de>
2019-11-17 17:42:41 +0100

    CamReady

--- Camera.gd
+++ Camera.gd
@@ -1,8 +1,13 @@
 extends Camera2D
 
-func _input(event: InputEvent) -> void:
-     if event is InputEventScreenDrag:
-           $".".offset -= event.relative
+onready var camera = $"."
+var max_downscroll = 1628 - (1920/3)
 
-
+func _input(event: InputEvent):
+	if event is InputEventScreenDrag:
+		camera.offset.y = max(   min(camera.offset.y - event.relative.y, max_downscroll),   0)
+		#compressed version of:
+		#camera.offset.y = camera.offset.y - event.relative.y  	#compute swipe camera drag
+		#camera.offset.y = max(camera.offset.y, 0)				#upper drag limit
+		#camera.offset.y = min(camera.offset.y, max_downscroll)	#lower drag limit
 
